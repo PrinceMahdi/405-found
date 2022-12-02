adcb26fe8930d647c8ea29300fd9c23b7a5a4ffe
Nataliia Sokolova <natalia.sokolova.ca@gmail.com>
2022-12-01 20:47:15 -0800

    add homepage style

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -3,9 +3,13 @@ import "./HomePage.scss";
 const HomePage = () => {
   return (
     <>
-    
+      <main className="main">
+        <>
+          <div className="search"></div>
+        </>
+      </main>
     </>
-  )
+  );
 };
 
 export default HomePage;
