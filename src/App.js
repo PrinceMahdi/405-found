bda056f37f46d664809650be09f1d469e140586f
Mahdi Shirmohammadi <shirmohammadi2000@gmail.com>
2022-12-02 12:49:45 -0800

    finished the profile section

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import Sidebar from "./Components/Sidebar/Sidebar";
 import Profile from "./Pages/Profile/Profile";
 import NewPost from "./Pages/NewPost/NewPost";
 
-
 const App = () => {
   return (
     <>
